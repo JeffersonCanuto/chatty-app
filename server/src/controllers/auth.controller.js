ac56b4bc4fb585b7f080afc0ed79198b0f719dbf
jefferson <jeffersondscanuto@gmail.com>
2025-04-13 14:00:52 -0300

    feat: create controller to sign up new user with their credentials

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,11 +1,58 @@
-export const signup = (req, res) => {
-    res.send("signup route...");
+import User from "../models/user.model.js";
+import generateJwtToken from "../lib/utils.js";
+
+import bcrypt from "bcryptjs";
+
+/* CREATE - Sign up a new user with their credentials */
+export const signup = async(req, res) => {
+    const { fullName, email, password } = req.body;
+    
+    try {
+        // Check whether password is valid (>=6 characters long)
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long'});
+        }
+
+        const user = await User.findOne({ email });
+
+        // Check whether user does not exist in the database already
+        if (user) return res.status(400).json({ message: 'Email already exists'});
+
+        // Hash password to store it in the database
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create a new user in the database
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            // Generate JWT token
+            generateJwtToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({ message: 'Invalid user data'});
+        }
+    } catch(error) {
+        console.error(`Error while signing up: ${error}`);
+        return res.status(500).json({ message: 'Internal Server Error'});
+    }
 };
 
 export const login = (req, res) => {
-    res.send("login route...");
+    res.send('login route...');
 };
 
 export const logout = (req, res) => {
-    res.send("logout route...");
+    res.send('logout route...');
 };
