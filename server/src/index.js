c4124800598986bb2a00e4277d4cb8c6d9f81295
jefferson <jeffersondscanuto@gmail.com>
2025-04-13 13:57:56 -0300

    chore: update files with minor changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.route.js";
 import connectDB from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 const PORT = process.env.APP_PORT;
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, error => {
     if (error) {
