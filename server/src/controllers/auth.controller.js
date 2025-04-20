301bc392cf53b4c55c131e33125ef414b50464a2
jefferson <jeffersondscanuto@gmail.com>
2025-04-20 14:22:06 -0300

    feat: create controller to log out user with their credentials

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -3,26 +3,26 @@ import generateJwtToken from "../lib/utils.js";
 
 import bcrypt from "bcryptjs";
 
-/* CREATE - Sign up a new user with their credentials */
+/* CREATE (POST) - Sign up a new user with the credentials provided at the Sign up form */
 export const signup = async(req, res) => {
     const { fullName, email, password } = req.body;
     
     try {
         // Check whether all fields are filled with proper values
         if (!fullName || !email || !password) {
-            return res.status(400).json({ message: "All fields are required"});
+            return res.status(400).json({ message: 'All fields are required' });
         }
 
         // Check whether password is valid (>=6 characters long)
         if (password.length < 6) {
-            return res.status(400).json({ message: 'Password must be at least 6 characters long'});
+            return res.status(400).json({ message: 'Password must be at least 6 characters long' });
         }
 
         const user = await User.findOne({ email });
 
         // Check whether user does not exist in the database already
         if (user) {
-            return res.status(400).json({ message: 'Email already exists'});
+            return res.status(400).json({ message: 'Email already exists' });
         }
 
         // Hash password to store it in the database
@@ -48,35 +48,36 @@ export const signup = async(req, res) => {
                 profilePicture: newUser.profilePicture
             });
         } else {
-            res.status(400).json({ message: 'Invalid user data'});
+            res.status(400).json({ message: 'Invalid user data' });
         }
     } catch(error) {
         console.error(`Error while signing up: ${error.message || error}`);
-        return res.status(500).json({ message: 'Internal Server Error'});
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
 };
 
+/* CREATE (POST) - Login user with the credentials provided at the Login form */
 export const login = async(req, res) => {
     const { email, password } = req.body;
 
     try {
         // Check whether email and password are provided
         if (!email || !password) {
-            return res.status(400).json({ message: "Email and password are required "});
+            return res.status(400).json({ message: 'Email and password are required' });
         }
 
         // Check whether user exists by email
         const user = await User.findOne({ email });
 
         if (!user) {
-            return res.status(400).json({ message: "Invalid credentials"});
+            return res.status(400).json({ message: 'Invalid credentials' });
         }
 
         // Check whether password is correct
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
         if (!isPasswordCorrect) {
-            return res.status(400).json({ message: "Invalid credentials"})
+            return res.status(400).json({ message: 'Invalid credentials' })
         }
 
         generateJwtToken(user._id, res)
@@ -89,10 +90,17 @@ export const login = async(req, res) => {
         });
     } catch(error) {
         console.error(`Error while logging in: ${error.message || error}`);
-        return res.status(500).json({ message: "Internal Server Error"});
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
 };
 
-export const logout = (req, res) => {
-    res.send('logout route...');
+/* CREATE (POST) - Logout user by removing the existing JWT Token from the Cookies */
+export const logout = (_, res) => {
+    try {
+        res.cookie('jwtToken', '', { maxAge: 0 });
+        return res.status(200).json({ message: 'Logged out successfully' });
+    } catch(error) {
+        console.error(`Error while logging out: ${error.message || error}`);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
 };
