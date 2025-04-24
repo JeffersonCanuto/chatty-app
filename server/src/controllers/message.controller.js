417e3d9d20c491f1039d3ae1f54d86d68f0ed4cd
jefferson <jeffersondscanuto@gmail.com>
2025-04-24 19:10:38 -0300

    feat: implement route and controller for send messages feature

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 /* FETCH (GET) - Retrieve all users/contacts for sidebar */
 export const getUsersForSidebar = async(req, res) => {
@@ -30,6 +31,39 @@ export const getMessages = async(req, res) => {
         return res.status(200).json(messages);
     } catch(error) {
         console.error(`Error while fetching messages between users: ${error.message || error}`);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+/* CREATE (POST) - Send text or image message to another user */
+export const sendMessage = async(req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        
+        let imageUrl;
+
+        if (image) {
+            // Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        // Save message in the database
+        await new Message.save();
+
+        // TO-DO: Implement real time functionality goes here => socket.io
+        return res.status(201).json(newMessage);
+    } catch(error) {
+        console.error(`Error while sending message: ${error.message || error}`);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 }
--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import protectRoute from "../middleware/auth.middleware.js";
 
-import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
 router.get('/:id', protectRoute, getMessages);
+router.post('/send/:id', protectRoute, sendMessage);
 
 export default router;
