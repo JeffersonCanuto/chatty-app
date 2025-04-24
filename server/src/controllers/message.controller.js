a63545e1ff1b81daa837e1627f0681dc95bcc005
jefferson <jeffersondscanuto@gmail.com>
2025-04-24 18:57:39 -0300

    feat: implement route and controller for get messages feature

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 /* FETCH (GET) - Retrieve all users/contacts for sidebar */
 export const getUsersForSidebar = async(req, res) => {
@@ -9,6 +10,26 @@ export const getUsersForSidebar = async(req, res) => {
         res.status(200).json(filteredUsers);
     } catch(error) {
         console.error(`Error while retrieving users for sidebar: ${error.message || error}`);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+/* FETCH (GET) - Retrieve all messages between different users */
+export const getMessages = async(req, res) => {
+    try {
+        const { id:userToChatId } = req.params;
+        const currentId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: currentId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: currentId}
+            ]
+        });
+        
+        return res.status(200).json(messages);
+    } catch(error) {
+        console.error(`Error while fetching messages between users: ${error.message || error}`);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 }
--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
+
 import protectRoute from "../middleware/auth.middleware.js";
+
+import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
 
 const router = Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
+router.get('/:id', protectRoute, getMessages);
 
 export default router;
