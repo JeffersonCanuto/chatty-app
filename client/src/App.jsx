c313cf3aeebb263deda6431160427d29a4f70354
jefferson <jeffersondscanuto@gmail.com>
2025-04-25 22:43:09 -0300

    feat: create base files for pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
-import "./index.css";
+
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
 	return (
-		<div>Heya</div>
+		<div>
+			<Navbar />
+
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/settings" element={<SettingsPage />} />
+				<Route path="/profile" element={<ProfilePage />} />
+			</Routes>
+		</div>
 	);
 }
 
